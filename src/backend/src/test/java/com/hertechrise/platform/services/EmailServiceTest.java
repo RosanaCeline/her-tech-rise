de96e4bd9b511923809a3d5950f2a2eea2057b21
Cláudia <claudiasouzaf7@gmail.com>
2025-07-19 15:16:27 -0300

    Atualizações locais

--- src/backend/src/test/java/com/hertechrise/platform/services/EmailServiceTest.java
+++ src/backend/src/test/java/com/hertechrise/platform/services/EmailServiceTest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
-@ActiveProfiles("mailtrap")
+@ActiveProfiles("email")
 class EmailServiceTest {
 
     //Testando envio de email personalizados com mailtrap servidor de teste SMTP
