aa388121696fbacf9d69263958921ae95043f5b2
Rosana Celine <celine.rosana@gmail.com>
2025-07-21 18:07:07 -0300

    ci: add and modify configurations to deploy backend with database

--- src/backend/src/main/resources/db/migration/V10__Create_Table_Professional.sql
+++ src/backend/src/main/resources/db/migration/V10__Create_Table_Professional.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS `professional` (
-    `user_id` BIGINT PRIMARY KEY,
-    `cpf` VARCHAR(14) UNIQUE NOT NULL,
-    `birth_date` DATE NOT NULL,
-    `technology` VARCHAR(80),
-    `biography` VARCHAR(1000),
-    `external_link` VARCHAR(100),
-    CONSTRAINT fk_prof_user FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
-)  ENGINE=INNODB;
+CREATE TABLE IF NOT EXISTS professional (
+    user_id BIGINT PRIMARY KEY,
+    cpf VARCHAR(14) UNIQUE NOT NULL,
+    birth_date DATE NOT NULL,
+    technology VARCHAR(80),
+    biography VARCHAR(1000),
+    external_link VARCHAR(100),
+    CONSTRAINT fk_prof_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
--- src/backend/src/main/resources/db/migration/V9__Create_Table_User_Role.sql
+++ src/backend/src/main/resources/db/migration/V9__Create_Table_User_Role.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS user_role (
-	`user_id` BIGINT NOT NULL,
-	`role_id` BIGINT NOT NULL,
-	PRIMARY KEY (`user_id`, `role_id`),
-	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
-	FOREIGN KEY (`role_id`) REFERENCES `role`(`id`)
-) ENGINE=InnoDB;
+    user_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE RESTRICT
+);
