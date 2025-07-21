e3bfe0ae7071d6e703a01b2282261fe266b3d141
Rosana Celine <celine.rosana@gmail.com>
2025-07-21 18:20:22 -0300

    ci: add verification if its local build

--- src/backend/src/main/java/com/hertechrise/platform/Startup.java
+++ src/backend/src/main/java/com/hertechrise/platform/Startup.java
@@ -10,13 +10,15 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class Startup {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		if (System.getenv("SPRING_PROFILES_ACTIVE") == null) {
+			Dotenv dotenv = Dotenv.load();
 
-		dotenv.entries().forEach(entry -> {
-			if (System.getenv(entry.getKey()) == null) {
-				System.setProperty(entry.getKey(), entry.getValue());
-			}
-		});
+			dotenv.entries().forEach(entry -> {
+				if (System.getenv(entry.getKey()) == null) {
+					System.setProperty(entry.getKey(), entry.getValue());
+				}
+			});
+		}
 
 		SpringApplication.run(Startup.class, args);
 	}
