210e6bb2c1711fc396ca95ce00842a27a15cb1a2
laisdeveloper <laiscc575@gmail.com>
2025-07-23 13:45:39 -0300

    fix: reviewing routes to use Navigate

--- src/frontend/src/components/layout/Footer/Footer.jsx
+++ src/frontend/src/components/layout/Footer/Footer.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import logo from '../../../assets/logo/LogoName.png';
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear()
   return (
     <footer className="w-full bg-(--purple-secundary) flex flex-col md:flex-row justify-between items-center px-20 py-6 text-white text-m lg:flex-row">
-      <img  src={logo} 
+      <a
+        href="https://github.com/RosanaCeline/her-tech-rise"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hidden lg:block w-auto max-w-[20%]"
+      >
+        <img  src={logo} 
             alt="Logo Her Tech Rise" 
             className="hidden lg:block w-auto max-w-[20%]" />
+      </a>
       <span className="mt-4 md:mt-0 w-full text-center lg:text-right lg:w-auto">
-        © Copyright Her Tech Rise 2025. Todos os direitos reservados.
+        © Copyright Her Tech Rise {currentYear}. Todos os direitos reservados.
       </span>
     </footer>
   );
