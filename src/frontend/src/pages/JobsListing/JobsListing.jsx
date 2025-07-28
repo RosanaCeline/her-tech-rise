597afc5aa36d6ab898c3dcf777ec854c21f01fed
thalytalima211 <thalytalima211@gmail.com>
2025-07-28 13:46:38 -0300

    feat: implements responsive design

--- src/frontend/src/pages/JobsListing/JobsListing.jsx
+++ src/frontend/src/pages/JobsListing/JobsListing.jsx
@@ -52,8 +52,10 @@ export default function JobsListing(){
                     </BtnCallToAction>
                 </div>
                 <section className="flex flex-col my-3 gap-y-3">
-                    {jobs.map((job) => <CompanyJobDetails key={job.id} job={job} handleDeactivate={handleDeactivate} 
-                    setManageJobModal={setManageJobModal} setJobFormData={setJobFormData}/>)}
+                    {jobs.length > 0 
+                    ? jobs.map((job) => <CompanyJobDetails key={job.id} job={job} handleDeactivate={handleDeactivate} 
+                    setManageJobModal={setManageJobModal} setJobFormData={setJobFormData}/>)
+                    : <p className="mx-auto mt-4">Sua empresa ainda não publicou nenhuma vaga de emprego</p>}
                 </section>
             </div>
             {manageJobModal === 'new' || manageJobModal === 'edit' ? (
@@ -88,54 +90,65 @@ function CompanyJobDetails({job, setManageJobModal, setJobFormData, handleDeacti
             <div className="">
                 <img src={job.companyProfilePic} className="w-full object-cover rounded-xl"/>
             </div>
-            <div className="text-xs pl-4 col-span-6">
-                <p className="font-bold">{job.title}</p>
-                <p className="font-bold">{job.companyName}</p>
+            <div className="text-xs pl-4 col-span-9 flex flex-col lg:flex-row w-full">
+                <div className="w-7/9 pr-6"> 
+                    <p className="font-bold">{job.title}</p>
+                    <p className="font-bold">{job.companyName}</p>
 
-                <p className="font-bold">Sobre a vaga:</p>
-                {job.description}
+                    <p className="font-bold">Sobre a vaga:</p>
+                    {job.description}
 
-                <p className="font-bold">Requisitos:</p>
-                {job.requirements}
+                    <p className="font-bold">Requisitos:</p>
+                    {job.requirements}
 
-                <p className="font-bold">Tipo de contrato: <span className="font-normal">{contractTypes[job.contractType]}</span></p>
+                    <p className="font-bold">Tipo de contrato: <span className="font-normal">{contractTypes[job.contractType]}</span></p>
 
-                <div className="grid grid-cols-3 mt-3">
-                    <div>
-                        <p className="font-bold">Modalidade:</p>
-                        <p className="flex items-center gap-1"><House size={18}/>{jobModels[job.jobModel]}</p>
+                    <div className="grid sm:grid-cols-3 mt-3">
+                        <div>
+                            <p className="font-bold">Modalidade:</p>
+                            <p className="flex items-center gap-1"><House size={18}/>{jobModels[job.jobModel]}</p>
+                        </div>
+                        <div>
+                            <p className="font-bold">Localização:</p>
+                            <p className="flex items-center gap-1"><MapPin size={18}/>{job.location}</p>
+                        </div>
+                        <div>
+                            <p className="font-bold">Data-limite:</p>
+                            <p className="flex items-center gap-1"><CalendarDays size={18}/>{new Date(job.applicationDeadline).toLocaleDateString('pt-BR')}</p>
+                        </div>
                     </div>
-                    <div>
-                        <p className="font-bold">Localização:</p>
-                        <p className="flex items-center gap-1"><MapPin size={18}/>{job.location}</p>
-                    </div>
-                    <div>
-                        <p className="font-bold">Data-limite:</p>
-                        <p className="flex items-center gap-1"><CalendarDays size={18}/>{new Date(job.applicationDeadline).toLocaleDateString('pt-BR')}</p>
-                    </div>
+
+                    {!job.isActive && <p>Vaga desativada</p>}
                 </div>
+                <div className="lg:w-2/9 text-sm flex flex-col md:flex-row lg:flex-col gap-y-3 items-end justify-around mt-4 lg:mt-0">
+                    <div className="lg:ml-auto">
+                    <button className="border border-(--purple-primary) text-(--purple-primary) cursor-pointer rounded-2xl py-2 px-6
+                    transition duration-300 hover:bg-(--purple-primary) hover:text-white hover:scale-110"
+                    onClick={async () => {
+                        const jobDetail = await companyJobPostingDetail(job.id)
+                        setJobFormData({
+                            ...jobDetail,
+                            salaryMin: maskField('money', Number(jobDetail.salaryMin).toFixed(2)),
+                            salaryMax: maskField('money', Number(jobDetail.salaryMax).toFixed(2))
+                        })
+                        setManageJobModal('edit')
+                    }}>
+                        Editar
+                    </button></div>
 
-                {!job.isActive && <p>Vaga desativada</p>}
-            </div>
-            <div className="col-span-3 text-xs flex flex-col gap-y-3 content-between">
-                <div className="ml-auto"><BtnCallToAction variant="white" 
-                onClick={async () => {
-                    const jobDetail = await companyJobPostingDetail(job.id)
-                    setJobFormData({
-                        ...jobDetail,
-                        salaryMin: maskField('money', Number(jobDetail.salaryMin).toFixed(2)),
-                        salaryMax: maskField('money', Number(jobDetail.salaryMax).toFixed(2))
-                    })
-                    setManageJobModal('edit')
-                }}>
-                    Editar
-                </BtnCallToAction></div>
-                <div className="ml-auto"><BtnCallToAction variant="white">
-                    Ver candidaturas
-                </BtnCallToAction></div>
-                {job.isActive && <div className="ml-auto"><BtnCallToAction onClick={() => handleDeactivate(job.id)}>
-                    Desativar
-                </BtnCallToAction></div>}
+                    <div className="lg:ml-auto">
+                    <button className="border border-(--purple-primary) text-(--purple-primary) cursor-pointer rounded-2xl py-2 px-6
+                    transition duration-300 hover:bg-(--purple-primary) hover:text-white hover:scale-110">
+                        Ver candidaturas
+                    </button></div>
+
+                    {job.isActive && <div className="lg:ml-auto"> 
+                    <button className="bg-(--purple-action) text-white cursor-pointer rounded-2xl py-2 px-6
+                    transition duration-300 hover:bg-(--purple-primary) hover:text-white hover:scale-110"
+                    onClick={() => handleDeactivate(job.id)}>
+                        Desativar
+                    </button></div>}
+                </div>
             </div>
         </div>
     )
