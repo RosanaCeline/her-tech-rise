8cb167d3cd87863eb654e20aa2b2a3e5ac9cb919
Cláudia <claudiasouzaf7@gmail.com>
2025-07-30 23:06:00 -0300

    add JobApplicationServiceTest

--- src/backend/src/test/java/com/hertechrise/platform/services/JobApplicationServiceTest.java
+++ src/backend/src/test/java/com/hertechrise/platform/services/JobApplicationServiceTest.java
@@ -1,6 +1,9 @@
 package com.hertechrise.platform.services;
-
+import com.hertechrise.platform.services.*;
+import com.hertechrise.platform.repository.JobPostingRepository;
+import com.hertechrise.platform.model.JobApplication;
 import com.hertechrise.platform.config.DotenvInitializer;
+import com.hertechrise.platform.data.dto.response.JobPostingResponseDTO;
 import com.hertechrise.platform.data.dto.request.JobApplicationRequestDTO;
 import com.hertechrise.platform.data.dto.request.JobPostingRequestDTO;
 import com.hertechrise.platform.exception.InvalidUserTypeException;
@@ -8,7 +11,23 @@ import com.hertechrise.platform.integrationtests.testcontainers.AbstractIntegrat
 import com.hertechrise.platform.model.*;
 import com.hertechrise.platform.repository.*;
 import jakarta.persistence.EntityManager;
-import jakarta.transaction.Transactional;
+import com.hertechrise.platform.data.dto.request.JobApplicationRequestDTO;
+import com.hertechrise.platform.data.dto.response.*;
+import com.hertechrise.platform.model.JobApplication;
+import com.hertechrise.platform.model.JobPosting;
+import com.hertechrise.platform.model.Professional;
+import com.hertechrise.platform.model.User;
+import com.hertechrise.platform.repository.JobApplicationRepository;
+import com.hertechrise.platform.repository.JobPostingRepository;
+import com.hertechrise.platform.repository.ProfessionalRepository;
+import com.hertechrise.platform.utils.HibernateUtils;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -58,6 +77,8 @@ class JobApplicationServiceTest extends AbstractIntegrationTest {
     private CompanyRepository companyRepository;
     @Autowired
     private JobPostingService jobPostingService;
+    @Autowired
+    private JobApplicationService jobApplicationService;
 
     // criando : User/professional
     private Professional createTestProfessioanal(String name, String email, String technology, String biografy, String handle) {
@@ -124,16 +145,12 @@ class JobApplicationServiceTest extends AbstractIntegrationTest {
     @DisplayName("Deve permitir que o usuário logado (profissional) se candidate a um vaga ")
     @Test
     void applyToJobSuccess() throws IOException {
-        Professional professional = createTestProfessioanal("Cláudia ", "claudia@email.com",
-                "Spring Boot", "Biografia teste", "@claudia"
-
-        );
-        UsernamePasswordAuthenticationToken auth =
-                new UsernamePasswordAuthenticationToken(professional.getUser(), null, List.of());
-        SecurityContextHolder.getContext().setAuthentication(auth);
-
         // anuncio da vaga
         Company company = createTestComapany("HerTechRise", "hertech@email.com", "@hertech");
+        UsernamePasswordAuthenticationToken auth1 =
+                new UsernamePasswordAuthenticationToken(company.getUser(), null, List.of());
+        SecurityContextHolder.getContext().setAuthentication(auth1);
+
         JobPosting job = new JobPosting();
         job.setCompany(company);
         job.setTitle("Programador Júnior");
@@ -148,29 +165,192 @@ class JobApplicationServiceTest extends AbstractIntegrationTest {
         job.setApplicationDeadline(LocalDate.now().plusWeeks(2));
         jobPostingRepository.save(job);
 
+        Professional professional = createTestProfessioanal("Cláudia ", "claudia@email.com",
+                "Spring Boot", "Biografia teste", "@claudia"
+
+        );
+        UsernamePasswordAuthenticationToken auth =
+                new UsernamePasswordAuthenticationToken(professional.getUser(), null, List.of());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
         InputStream inputStream = getClass().getResourceAsStream("/curriculoTest.pdf");
         MultipartFile multipartFile = new MockMultipartFile(
-                "file",
+                "resume",
                 "curriculoTest.pdf",
-                "arquivo/pdf",
+                "application/pdf",
                 inputStream
         );
         // Criar DTO de requisição
-        JobApplicationRequestDTO request = new JobApplicationRequestDTO(
+        JobApplicationRequestDTO applicationRequest = new JobApplicationRequestDTO(
                 job.getId(),
                 "https://github.com/claudia-souza",
                 "https://claudia.dev",
                 multipartFile
         );
-        // apenas para não quebrar o teste
-        //CandidateApplicationDetailsResponseDTO response = jobPostingService.applyToJob(request);
 
+        CandidateApplicationDetailsResponseDTO response = jobApplicationService.applyToJob(applicationRequest);
+        assertNotNull(response);
+        assertEquals("Programador Júnior", response.jobTitle());
+        assertTrue(response.resumeUrl().contains("cloudinary.com"));
+
+        // Verificar se a aplicação foi salva no banco
+        List<JobApplication> apps = jobApplicationRepository.findAll();
+        assertEquals(1, apps.size());
+
+        JobApplication savedApp = apps.get(0);
+        assertEquals(job.getId(), savedApp.getJobPosting().getId());
+        assertEquals(professional.getUserId(), savedApp.getProfessional().getUserId());
     }
-
+    @DisplayName("Deve lançar EntityNotFoundException o para vaga não encontarda")
     @Test
-    void getMyApplicationDetails() {
+    void applyToJobSuccessThrowEntityNotFoundException() throws IOException {
+        Professional professional = createTestProfessioanal("Cláudia ", "claudia@email.com",
+                "Spring Boot", "Biografia teste", "@claudia"
+
+        );
+        UsernamePasswordAuthenticationToken auth =
+                new UsernamePasswordAuthenticationToken(professional.getUser(), null, List.of());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+
+        InputStream inputStream = getClass().getResourceAsStream("/curriculoTest.pdf");
+        MultipartFile multipartFile = new MockMultipartFile(
+                "resume",
+                "curriculoTest.pdf",
+                "application/pdf",
+                inputStream
+        );
+        // Criar DTO de requisição
+        JobApplicationRequestDTO applicationRequest = new JobApplicationRequestDTO(
+                999L,
+                "https://github.com/claudia-souza",
+                "https://claudia.dev",
+                multipartFile
+        );
+        EntityNotFoundException exception = assertThrows(
+                EntityNotFoundException.class,
+                () -> jobApplicationService.applyToJob(applicationRequest) );
+
+        assertEquals("Vaga não encontrada.", exception.getMessage());
+
+    }
+    @DisplayName("Deve lançar EntityNotFoundException para quando usuário que se candidatou não é profissional")
+    @Test
+    void applyToJob_ShouldThrowException_NotProfessional() throws IOException {
+        Company company = createTestComapany("HerTechRise", "hertech@email.com", "@hertech");
+        UsernamePasswordAuthenticationToken auth1 =
+                new UsernamePasswordAuthenticationToken(company.getUser(), null, List.of());
+        SecurityContextHolder.getContext().setAuthentication(auth1);
+
+        InputStream inputStream = getClass().getResourceAsStream("/curriculoTest.pdf");
+        MultipartFile multipartFile = new MockMultipartFile(
+                "resume",
+                "curriculoTest.pdf",
+                "application/pdf",
+                inputStream
+        );
+        // Criar DTO de requisição
+        JobApplicationRequestDTO applicationRequest = new JobApplicationRequestDTO(
+                999L,
+                "https://github.com/claudia-souza",
+                "https://claudia.dev",
+                multipartFile
+        );
+        EntityNotFoundException exception = assertThrows(
+                EntityNotFoundException.class,
+                () -> jobApplicationService.applyToJob(applicationRequest)
+        );
+        assertEquals("Usuário logado não é um profissional.", exception.getMessage());
+
+    }
+    @DisplayName("Deve retornar detalhes de uma candidatura")
+    @Test
+    void getMyApplicationDetailsSuccess() throws IOException {
+        Company company = createTestComapany("HerTechRise", "hertech@email.com", "@hertech");
+        UsernamePasswordAuthenticationToken auth1 =
+                new UsernamePasswordAuthenticationToken(company.getUser(), null, List.of());
+        SecurityContextHolder.getContext().setAuthentication(auth1);
+
+        JobPosting job = new JobPosting();
+        job.setCompany(company);
+        job.setTitle("Programador Júnior");
+        job.setDescription("Vaga de programador");
+        job.setRequirements("HTML e CSS");
+        job.setLocation("Tianguá");
+        job.setJobModel(JobModel.PRESENCIAL);
+        job.setSalaryMin(BigDecimal.valueOf(2000));
+        job.setSalaryMax(BigDecimal.valueOf(2500));
+        job.setContractType(JobContractType.CLT);
+        job.setJoblevel(JobLevel.JUNIOR);
+        job.setApplicationDeadline(LocalDate.now().plusWeeks(2));
+        jobPostingRepository.save(job);
+
+        Professional professional = createTestProfessioanal("Cláudia ", "claudia@email.com",
+                "Spring Boot", "Biografia teste", "@claudia"
+
+        );
+        UsernamePasswordAuthenticationToken auth =
+                new UsernamePasswordAuthenticationToken(professional.getUser(), null, List.of());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        // Simula upload do currículo
+        InputStream inputStream = getClass().getResourceAsStream("/curriculoTest.pdf");
+        MultipartFile multipartFile = new MockMultipartFile(
+                "resume",
+                "curriculoTest.pdf",
+                "application/pdf",
+                inputStream
+        );
+        JobApplicationRequestDTO applicationRequest = new JobApplicationRequestDTO(
+                job.getId(),
+                "https://github.com/claudia-souza",
+                "https://claudia.dev",
+                multipartFile
+        );
+        CandidateApplicationDetailsResponseDTO applicationResponse = jobApplicationService.applyToJob(applicationRequest);
+
+
+        CandidateApplicationDetailsResponseDTO response = jobApplicationService.getMyApplicationDetails(applicationResponse.applicationId());
+
+        assertNotNull(response);
+        assertEquals(applicationResponse.applicationId(), response.applicationId());
+        assertEquals(professional.getUserId(), response.applicantId());
+        assertEquals("Cláudia ", response.applicantName());
+        assertEquals("Spring Boot", response.applicantTechnology());
+        assertEquals("88900000000", response.applicantPhone());
+        assertEquals("claudia@email.com", response.applicantEmail());
+        assertNotNull(response.applicantProfilePic());
+        assertTrue(response.resumeUrl().contains("cloudinary.com"));
+        assertEquals("https://github.com/claudia-souza", response.githubLink());
+        assertEquals("https://claudia.dev", response.portfolioLink());
+        assertEquals("Programador Júnior", response.jobTitle());
+        assertNotNull(response.appliedAt());
+        assertEquals(job.getApplicationDeadline(), response.applicationDeadline());
+        assertEquals(job.isActive(),true);
+        assertEquals(job.isExpired(),false);
+        assertEquals("HerTechRise", response.companyName());
+        assertNotNull(response.companyProfilePic());
+        assertEquals(company.getUserId(), response.companyUserId());
+    }
+    @DisplayName("Deve lançar EntityNotFoundException para detalhes de candidatura não existente ")
+    @Test
+    void getMyApplicationDetailsThrowEntityNotFoundException(){
+        Professional professional = createTestProfessioanal("Cláudia ", "claudia@email.com",
+                "Spring Boot", "Biografia teste", "@claudia"
+
+        );
+        UsernamePasswordAuthenticationToken auth =
+                new UsernamePasswordAuthenticationToken(professional.getUser(), null, List.of());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        EntityNotFoundException exception = assertThrows(
+                EntityNotFoundException.class,
+                () -> jobApplicationService.getMyApplicationDetails(999L)
+        );
+
+        assertEquals("Candidatura não encontrada.", exception.getMessage());
+
     }
 
+    /**
     @Test
     void getReceivedApplicationDetails() {
     }
@@ -186,4 +366,5 @@ class JobApplicationServiceTest extends AbstractIntegrationTest {
     @Test
     void deleteById() {
     }
+    **/
 }
