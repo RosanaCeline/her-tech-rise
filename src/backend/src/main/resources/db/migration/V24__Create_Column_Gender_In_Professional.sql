95643913847604df3e892c26f75c0ef7e847ebf5
Rosana Celine <celine.rosana@gmail.com>
2025-08-01 12:09:16 -0300

    refactor: change gender type in database

--- src/backend/src/main/resources/db/migration/V24__Create_Column_Gender_In_Professional.sql
+++ src/backend/src/main/resources/db/migration/V24__Create_Column_Gender_In_Professional.sql
@@ -1,19 +1,5 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'professional_gender') THEN
-        CREATE TYPE professional_gender AS ENUM (
-            'MULHER',
-            'HOMEM',
-            'PESSOA_NAO_BINARIA',
-            'PREFIRO_NAO_INFORMAR',
-            'OUTRO'
-        );
-    END IF;
-END
-$$;
+ALTER TABLE professional
+ADD COLUMN IF NOT EXISTS gender VARCHAR(20) CHECK (gender IN ('MULHER', 'HOMEM', 'PESSOA_NAO_BINARIA', 'PREFIRO_NAO_INFORMAR', 'OUTRO'));
 
 ALTER TABLE professional
-ADD COLUMN gender professional_gender;
-
-ALTER TABLE professional
-ADD COLUMN consent_gender_sharing BOOLEAN;
+ADD COLUMN IF NOT EXISTS  consent_gender_sharing BOOLEAN;
