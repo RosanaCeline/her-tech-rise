3dbddcd8bc27ff497ef794c2de438100c50c90ab
laisdeveloper <laiscc575@gmail.com>
2025-08-08 17:43:15 -0300

    fix: adjusting the visibility of shared posts, follow and unfollow on the timeline, more options on the post itself on the timeline

--- src/frontend/src/components/PopUp.jsx
+++ src/frontend/src/components/PopUp.jsx
@@ -1,6 +1,6 @@
 export default function PopUp({children}){
     return(
-         <div class="fixed inset-0 z-50 bg-black/40 backdrop-blur-sm flex items-center justify-center p-4">
+         <div className="fixed inset-0 z-50 bg-black/40 backdrop-blur-sm flex items-center justify-center p-4">
             <div class="bg-white rounded-2xl shadow-lg w-full text-center max-w-lg overflow-auto max-h-[90vh] p-6">
                 {children}
             </div>
