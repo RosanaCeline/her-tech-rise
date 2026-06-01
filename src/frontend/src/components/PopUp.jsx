fb257726de24db09ec0eef84551292233c180567
Rosana Celine <celine.rosana@gmail.com>
2026-06-01 10:56:12 -0300

    style: remove unnecessary scrolls in pop-up

--- src/frontend/src/components/PopUp.jsx
+++ src/frontend/src/components/PopUp.jsx
@@ -1,7 +1,14 @@
-export default function PopUp({children}){
-    return(
-         <div className="fixed inset-0 z-50 bg-black/40 backdrop-blur-sm flex items-center justify-center p-4">
-            <div className="bg-white rounded-2xl shadow-lg w-full text-center max-w-lg overflow-auto max-h-[90vh] p-6">
+import { useEffect } from "react"
+
+export default function PopUp({ children }) {
+    useEffect(() => {
+        document.body.classList.add('overflow-hidden')
+        return () => document.body.classList.remove('overflow-hidden')
+    }, [])
+
+    return (
+        <div className="fixed inset-0 z-50 bg-black/40 backdrop-blur-sm flex items-center justify-center p-4">
+            <div className="bg-white rounded-2xl shadow-lg w-full text-center max-w-xl max-h-[90vh] p-6 overflow-hidden">
                 {children}
             </div>
         </div>
